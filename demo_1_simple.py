da5e73f12d44429b12ac94149a61312460bb03a8
Riccardo Borgani <borgani@kth.se>
2020-11-20 15:04:23 +0100

    move to office computer

--- demo_1_simple.py
+++ demo_1_simple.py
@@ -1,38 +1,51 @@
+""" Create one output template and sample it.
 
-import numpy as np
+Connect one output port directly to one input port.
+"""
 import matplotlib.pyplot as plt
+import numpy as np
 
 from vivace import pulsed
 
+output_port = 3
+input_port = 1
 
-with pulsed.Pulsed(ext_ref_clk=False, force_reload=False, address="192.168.42.50", port=3490) as pls:
+# ADDRESS = "192.168.42.50"  # set address/hostname of Vivace here
+# PORT = 3490
+ADDRESS = "130.237.35.90"  # set address/hostname of Vivace here
+PORT = 42090
+EXT_REF = False  # set to True to use external 10 MHz reference
+
+with pulsed.Pulsed(ext_ref_clk=EXT_REF, address=ADDRESS, port=PORT) as pls:
     ######################################################################
-    # Select inputs to store and the duration of each store
-    pls.set_store_ports([1,])
-    pls.set_store_duration(1e-6)
+    # Select input ports to store and the duration of each store
+    pls.set_store_ports(input_port)
+    pls.set_store_duration(1e-6)  # 1 microsecond
 
     ######################################################################
-    # create a 512 sample long template on output 1
+    # create a 512-sample-long template on output_port
+    # make a sine wave with a Hanning window
     N = 512
-    t = np.arange(N)*0.25e-9
-    f = 55e6
-    s = np.sin(2*np.pi*f*t) * np.hanning(N)
-    template_1 = pls.setup_template(1, s)
+    t = np.arange(N) / pls.sampling_freq
+    freq = 55e6
+    data = np.sin(2 * np.pi * freq * t) * np.hanning(N)
+    template_1 = pls.setup_template(output_port, data)
 
     ######################################################################
     # define the sequence of pulses and data stores in time
-    # at time 1us, output the template and start a store window
-    pls.output_pulse(1e-6, template_1)
-    pls.store(1e-6)
-
+    # at time zero, output the template and start a store window
+    T = 0.0
+    pls.output_pulse(T, template_1)
+    pls.store(T)
 
     ######################################################################
     # Actually run the sequence, only run once with no averaging
-    t_arr, data = pls.perform_measurement(period=10e-6, repeat_count=1, num_averages=1)
+    t_arr, data = pls.perform_measurement(period=10e-6,
+                                          repeat_count=1,
+                                          num_averages=1)
 
-fig, ax = plt.subplots()
-ax.plot(t_arr, data[0, 0, :], label="store 0, port 0")
+fig, ax = plt.subplots(tight_layout=True)
+ax.plot(1e9 * t_arr, data[0, 0, :], label="store 0, port 0")
+ax.set_xlabel("Time [ns]")
 ax.legend()
 fig.show()
-
-
