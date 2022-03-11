28a007c8d62f71c6e2ee93c7018ed7fc772c401a
Riccardo Borgani <44963821+rikyborg@users.noreply.github.com>
2022-03-11 21:28:55 +0100

    Update symmetric_lockin_1.py

---
+++ symmetric_lockin_1.py
@@ -0,0 +1,173 @@
+# -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
+import numpy as np
+
+from presto.hardware import AdcFSample, AdcMode, DacFSample, DacMode
+from presto import lockin
+
+ADDRESS = "192.168.88.59"  # Presto's IP address
+
+# ******************************
+# *** define some parameters ***
+# ******************************
+
+INPUT_PORT = 1
+OUTPUT_PORT = 1  # can be a list
+
+
+
+# 1 MHz, demodulation rate
+DF = 1e6
+# 2 GHz, frequency of numerically-controlled oscillator for up/down-conversion
+NCO_FREQ = 2e9
+# 192 IF frequencies between 154 MHz and 345 MHz
+IF_FREQ_ARR = 250e6 + 1e6 * np.arange(-96, 96)
+NR_FREQS = len(IF_FREQ_ARR)
+# drive amplitude in full-scale units, i.e. 1.0 is 100%
+AMP = 0.707
+# equal amplitude on all tones in the comb
+AMP_ARR = np.full(NR_FREQS, AMP / NR_FREQS)
+# and random phase
+PHASE_ARR = 2 * np.pi * np.random.random_sample(NR_FREQS)
+
+# low-pass filter demodulated data with a 8-samples sliding window, ~100 kHz BW
+NBOX = 8
+# calculate mean and std in chunks of 250 "raw" measurements
+NSUM = 250
+# acquire 100 measurements of mean and std
+NR_MEAS = 100
+# there will be 100 * 250 = 25k "raw" measurements
+NR_RAW_MEAS = NR_MEAS * NSUM
+
+# add noise to output to improve linearity at small amplitudes
+DITHER = True
+# select analog output range (min 2_250 uA, max 40_500 uA)
+DAC_CURRENT = 40_500
+# select configuration for AD and DA converters
+CONVERTER_CONFIGURATION = {
+    "adc_mode": AdcMode.Mixed,  # use digital downconversion
+    "adc_fsample": AdcFSample.G4,  # 4 GS/s sampling rate
+    "dac_mode": DacMode.Mixed02,  # use digital upconversion
+    "dac_fsample": DacFSample.G6,  # 6 GS/s sampling rate
+}
+
+# ********************************
+# *** Initialize the interface ***
+# ********************************
+
+# This will connect to the hardware
+with lockin.SymmetricLockin(address=ADDRESS, **CONVERTER_CONFIGURATION) as lck:
+    assert lck.hardware is not None  # just to help the text editor with autocompletion
+
+    # ***********************************
+    # *** Configure hardware features ***
+    # ***********************************
+
+    # set digital-step attenuator on the input to 0 dB (max 27 dB)
+    lck.hardware.set_adc_attenuation(INPUT_PORT, 0.0)
+    # set variable output power on the output
+    lck.hardware.set_dac_current(OUTPUT_PORT, DAC_CURRENT)
+    # configure up- and down-conversion digital mixers
+    lck.hardware.configure_mixer(NCO_FREQ, in_ports=INPUT_PORT, out_ports=OUTPUT_PORT)
+
+    # **********************************
+    # *** Configure lock-in features ***
+    # **********************************
+
+    # demodulation rate
+    lck.set_df(DF)
+    # make outputs free-running (because we are not tuning the frequencies)
+    lck.set_phase_reset(False)
+    # add dithering to outputs
+    lck.set_dither(DITHER, OUTPUT_PORT)
+
+    # output a 100ns-wide trigger on digital port 1 every "summing window"
+    # i.e. every time a mean and std calculation is performed on a chunk
+    # of lock-in packets
+    lck.set_trigger_out(2, delay=0.0, width=100e-9)
+
+    # create an input-output group of tones
+    sg = lck.add_symmetric_group(INPUT_PORT, OUTPUT_PORT, NR_FREQS)
+    # and configure it with frequencies, amplitudes and phase
+    sg.set_frequencies(IF_FREQ_ARR).set_amplitudes(AMP_ARR).set_phases(PHASE_ARR)
+
+    # **********************
+    # *** Apply settings ***
+    # **********************
+
+    # actually upload parameters to the hardware and apply settings
+    lck.apply_settings()
+
+    # the frequency comb will be output starting from now!
+
+    # *****************************
+    # *** Perform measurements ****
+    # *****************************
+
+    # Acquire 25k lock-in packets at 1 MHz rate with ~100 kHz low-pass filter
+    _, data_raw = lck.get_pixels(
+        n=NR_RAW_MEAS,
+        summed=False,  # "raw" lock-in measurements
+        nbox=NBOX,  # low-pass filter
+    )[INPUT_PORT]
+
+    # Acquire 100 mean and std measurements at 4 kHz rate = 1 MHz / 250
+    _, data_mean, data_std = lck.get_pixels(
+        n=NR_MEAS,
+        nbox=NBOX,  # low-pass filter
+        summed=True,  # calculate mean and standard deviation...
+        nsum=NSUM,  # ...on chunks of 250 lock-in packets each
+    )[INPUT_PORT]
+
+    # *******************************************************
+    # *** Shut down outputs at the end of the measurement ***
+    # *******************************************************
+    sg.set_amplitudes(0.0)
+    lck.set_trigger_out(0)
+    lck.apply_settings()
+
+# Exited `with` block: connection to Presto is now closed
+
+# data_raw, data_mean and data_std are NumPy arrays
+# with shape (nr_measurements, nr_frequencies)
+assert data_raw.shape == (NR_RAW_MEAS, NR_FREQS)
+assert data_mean.shape == (NR_MEAS, NR_FREQS)
+assert data_std.shape == (NR_MEAS, NR_FREQS)
+# and complex data type for data_raw and data_mean, and real for data_std
+assert data_raw.dtype == np.complex128
+assert data_mean.dtype == np.complex128
+assert data_std.dtype == np.float64
+
+# *****************************
+# *** Analyze and plot data ***
+# *****************************
+
+# "manually" calculate mean and standard deviation of raw data
+# in chunks of 250 measurements
+# and compare to mean and std calculated by Presto
+manual_mean = np.zeros((NR_MEAS, NR_FREQS), np.complex128)
+manual_std = np.zeros((NR_MEAS, NR_FREQS), np.float64)
+data2 = np.reshape(data_raw, (NR_MEAS, NSUM, NR_FREQS))
+for i in range(NR_MEAS):
+    manual_mean[i] = np.mean(data2[i, :, :], axis=0)
+    manual_std[i] = np.std(data2[i, :, :], axis=0)
+
+# plot values for 1st frequency only
+FREQ_IDX = 0
+
+# time array for plotting
+time_arr = np.arange(NR_MEAS) * (NSUM / lck.get_df())
+
+fig1, ax1 = plt.subplots(3, 1, sharex=True, tight_layout=True)
+ax11, ax12, ax13 = ax1
+ax11.plot(1e6 * time_arr, manual_mean[:, FREQ_IDX].real, label="manual")
+ax11.plot(1e6 * time_arr, data_mean[:, FREQ_IDX].real, label="presto")
+ax12.plot(1e6 * time_arr, manual_mean[:, FREQ_IDX].imag)
+ax12.plot(1e6 * time_arr, data_mean[:, FREQ_IDX].imag)
+ax13.plot(1e6 * time_arr, manual_std[:, FREQ_IDX])
+ax13.plot(1e6 * time_arr, data_std[:, FREQ_IDX])
+ax11.set_ylabel("Mean, real")
+ax12.set_ylabel("Mean, imag")
+ax13.set_ylabel("Std")
+ax13.set_xlabel("Time [μs]")
+fig1.show()
